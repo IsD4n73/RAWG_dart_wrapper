ebe50b46d72f9c90319f959bb19614864fcb4329
Dante <68009431+IsD4n73@users.noreply.github.com>
2024-01-02 17:54:34 +0100

    Update rawg_utils.dart

--- lib/controller/rawg_utils.dart
+++ lib/controller/rawg_utils.dart
@@ -1,6 +1,8 @@
 import 'ordering.dart';
+import 'package:dio/dio.dart';
 
 class RawgUtils{
+  static final dio = Dio(); 
   static String baseUrl = "https://api.rawg.io/api";
 
   static void games(int page, Ordering? order, String? query){}
