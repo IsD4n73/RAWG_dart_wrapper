476de07d7c6e72279f35c3da1386613c802924c2
Dante <68009431+IsD4n73@users.noreply.github.com>
2024-01-02 19:04:13 +0100

    Update genre_controller.dart

--- lib/controller/genre_controller.dart
+++ lib/controller/genre_controller.dart
@@ -1,1 +1,25 @@
+import 'package:dio/dio.dart';
 
+
+
+Future<List<Genre>> loadGenre() async {
+  Response response = await RawgUtils.dio.get(RawgUtils.baseUrl + '/genres', queryParameters: {"key" : Rawg.key, "page_size": 100,},);
+
+  if(response.statusCode! = 200){
+    throw("error");
+  }
+
+  if(response.data["error"] != null || response.data["details"] != null){} 
+
+  
+  List<Genre> results = [];
+  if(response.data['results'] == null){
+   return [];
+  }
+  
+  response.data['results'].forEach((v) {
+    results.add(Genre.fromJson(v));
+  });
+  return results;
+  
+} 
