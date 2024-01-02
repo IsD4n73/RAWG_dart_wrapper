4fce6d3e66ca0735b26d09e3908fb64e26669805
Dante <68009431+IsD4n73@users.noreply.github.com>
2024-01-02 17:12:44 +0100

    Update RAWG_dart_wrapper.dart

--- lib/RAWG_dart_wrapper.dart
+++ lib/RAWG_dart_wrapper.dart
@@ -1,1 +1,9 @@
-export 'rawg.dart' show Rawg;
+library RAWG_dart_wrapper;
+
+export 'controller/rawg.dart' show Rawg;
+export 'models/game.dart';
+export 'models/achievement.dart';
+export 'models/errors.dart';
+export 'models/genre.dart';
+export 'models/ordering.dart';
+export 'models/platform.dart';
