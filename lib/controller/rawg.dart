3beffa9110523109f285c1ef5eecbd0d6ed1865b
Dante <68009431+IsD4n73@users.noreply.github.com>
2024-01-02 16:59:41 +0100

    Add files via upload

--- lib/controller/rawg.dart
+++ lib/controller/rawg.dart
@@ -1,1 +1,42 @@
+import 'ordering.dart';
+import 'rawg_utils.dart';
 
+class Rawg{
+  /// Api key for authentication
+  static String? key;
+  
+  Rawg({required String apiKey}){
+    key = apiKey;
+  }
+
+  /// Get a list of [Game] 
+  /// params: page, ordering
+  /// You can sort the results by the enum [Ordering]
+  /// return an empty list if the page does not exist
+  static void getGames({int page = 1, Ordering? order, String? query}){
+    RawgUtils.games(page, order, query);
+  }
+
+  /// Get a [Game] by ID with more details (achievement count, alias names, original name, website)
+  static void getGameDetails({required int id}){
+    RawgUtils.detail(id);
+  }
+
+  /// Get a list of [Genre]
+  static void getGenres(){
+    RawgUtils.genres();
+  }
+
+  /// Get a list of [Achievement]
+  /// params: page
+  /// return an empty list if the page does not exist
+  static void getAchievements({required int id, int page = 1}){
+    RawgUtils.achievement(page);
+  }
+
+  /// Get a list of [Platform]
+  static void getPlatforms(){
+    RawgUtils.platforms();
+  }
+  
+}
---
+++ lib/controller/rawg_utils.dart
@@ -0,0 +1,15 @@
+import 'ordering.dart';
+
+class RawgUtils{
+  static String baseUrl = "https://api.rawg.io/api";
+
+  static void games(int page, Ordering? order, String? query){}
+
+  static void detail(int id){}
+  
+  static void genres(){}
+  
+  static void achievement(int page){}
+  
+  static void platforms(){}
+}
