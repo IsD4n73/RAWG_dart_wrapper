b294c003f39f119d861b4a96532348d7a5fd143a
Dante <d.dante03@gmail.com>
2024-01-03 10:15:48 +0100

    upgrade import

--- lib/RAWG_dart_wrapper.dart
+++ lib/RAWG_dart_wrapper.dart
@@ -1,9 +1,9 @@
 library RAWG_dart_wrapper;
 
-export 'controller/rawg.dart';
-export 'models/game.dart';
-export 'models/achievement.dart';
+export 'controller/rawg.dart' show Rawg;
+export 'models/game.dart' show Game;
+export 'models/achievement.dart' show Achievement;
 export 'models/errors.dart';
-export 'models/genre.dart';
-export 'models/ordering.dart';
-export 'models/platform.dart';
+export 'models/genre.dart' show Genre;
+export 'models/ordering.dart' show Ordering;
+export 'models/platform.dart' show Platform;
